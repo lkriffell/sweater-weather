be8ffeee4df411e3e14bc5c921d2e72a6a57d161
Logan Riffell <lkriffell@gmail.com>
2020-11-08 11:28:33 -0700

    Update specs to check for numericality

--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -14,24 +14,24 @@ RSpec.describe 'forecast facade' do
             current_forecast_keys = {dt: Date,
                                     sunrise: Date,
                                     sunset: Date,
-                                    temp: Float,
+                                    temp: Integer,
                                     feels_like: Float,
                                     humidity: Integer,
                                     uvi: Float,
                                     visibility: Integer,
-                                    description: String,
+                                    conditions: String,
                                     icon: String
                                   }
 
             expect(forecast.current_weather[:dt].class).to eq(current_forecast_keys[:dt])
             expect(forecast.current_weather[:sunrise].class).to eq(current_forecast_keys[:sunrise])
             expect(forecast.current_weather[:sunset].class).to eq(current_forecast_keys[:sunset])
-            expect(forecast.current_weather[:temp].class).to eq(current_forecast_keys[:temp])
+            expect(forecast.current_weather[:temp].to_s.numeric?).to eq(true)
             expect(forecast.current_weather[:feels_like].class).to eq(current_forecast_keys[:feels_like])
             expect(forecast.current_weather[:humidity].class).to eq(current_forecast_keys[:humidity])
             expect(forecast.current_weather[:uvi].class).to eq(current_forecast_keys[:uvi])
             expect(forecast.current_weather[:visibility].class).to eq(current_forecast_keys[:visibility])
-            expect(forecast.current_weather[:conditions].class).to eq(current_forecast_keys[:description])
+            expect(forecast.current_weather[:conditions].class).to eq(current_forecast_keys[:conditions])
             expect(forecast.current_weather[:icon].class).to eq(current_forecast_keys[:icon])
           end
         end
@@ -80,16 +80,21 @@ RSpec.describe 'forecast facade' do
                                     icon: String
                                   }
 
-            forecast.hourly_weather.each do |day|
-              expect(day[:dt].class).to eq(hourly_forecast_keys[:dt])
-              expect(day[:wind_speed].class).to eq(hourly_forecast_keys[:wind_speed])
-              expect(day[:wind_direction].class).to eq(hourly_forecast_keys[:wind_direction])
-              expect(day[:conditions].class).to eq(hourly_forecast_keys[:conditions])
-              expect(day[:icon].class).to eq(hourly_forecast_keys[:icon])
+            forecast.hourly_weather.each do |hour|
+              expect(hour[:dt].class).to eq(hourly_forecast_keys[:dt])
+              expect(hour[:wind_speed].class).to eq(hourly_forecast_keys[:wind_speed])
+              expect(hour[:wind_direction].class).to eq(hourly_forecast_keys[:wind_direction])
+              expect(hour[:conditions].class).to eq(hourly_forecast_keys[:conditions])
+              expect(hour[:icon].class).to eq(hourly_forecast_keys[:icon])
             end
           end
         end
       end
     end
+  end
+end
+class String
+  def numeric?
+      Float(self) != nil rescue false
   end
 end
--- spec/services/forecast_service_spec.rb
+++ spec/services/forecast_service_spec.rb
@@ -1,34 +1,153 @@
 require 'rails_helper'
 
-RSpec.describe 'forecast facade' do
+RSpec.describe 'forecast service' do
   describe 'forecast_details' do
-    describe 'returns a forecast object for' do
-      it 'current_weather' do
-        VCR.use_cassette 'denver_co' do
-          params = {location: 'denver,co'}
-          location = MapFacade.location_details(params[:location])
+    describe 'returns a forecast json for' do
+      VCR.use_cassette 'denver_co' do
+        params = {location: 'denver,co'}
+        location = MapFacade.location_details(params[:location])
 
-          VCR.use_cassette 'denver_co_current_forecast' do
-            forecast = ForecastService.forecast_details(location)
-            forecast = JSON.parse(forecast.body, symbolize_names: true)
+        VCR.use_cassette 'denver_co_current_forecast' do
+          forecast = ForecastService.forecast_details(location)
+          forecast = JSON.parse(forecast.body, symbolize_names: true)
+
+          it 'doesnt include :minutely' do
             response_keys = {current: Hash, hourly: Array, daily: Array}
-            forecast_keys = {dt: Date,
-                                    sunrise: Date,
-                                    sunset: Date,
-                                    temp: Float,
-                                    feels_like: Float,
-                                    humidity: Integer,
-                                    uvi: Float,
-                                    visibility: Integer,
-                                    description: String,
-                                    icon: String
-                                  }
+            response_keys.keys.each { |key|  expect(forecast.keys).to include(key) }
+            expect(forecast.keys).to_not include(:minutely)
+          end
 
-            expect(forecast.keys).to include(response_keys.keys)
+          it 'current_weather' do
+            current = {
+                       dt:Integer,
+                       sunrise:Integer,
+                       sunset:Integer,
+                       temp:Integer,
+                       feels_like:Float,
+                       pressure:Integer,
+                       humidity:Integer,
+                       dew_point:Float,
+                       uvi:Float,
+                       clouds:Integer,
+                       visibility:Integer,
+                       wind_speed:Float,
+                       wind_deg:Integer,
+                       wind_gust:Float,
+                       weather: Array,
+                      }
+             weather = {
+                        id:Integer,
+                        main:String,
+                        description:String,
+                        icon:String
+                       }
 
+             expect(forecast[:current].keys).to eq(current.keys)
+             expect(forecast[:current][:weather][0].keys).to eq(weather.keys)
+             expect(forecast[:current][:weather][0].keys).to eq(weather.keys)
+
+             forecast[:current].each do |key, value|
+               if key != :temp
+                 expect(value.class).to eq(current[key])
+               else
+                 expect(value.to_s.numeric?).to eq(true)
+               end
+             end
+             forecast[:current][:weather][0].each do |key, value|
+               expect(value.class).to eq(weather[key])
+             end
+          end
+
+          it 'daily_weather' do
+            daily = {
+                      dt: Integer,
+                      sunrise: Integer,
+                      sunset: Integer,
+                      temp: Hash,
+                      feels_like: Hash,
+                      pressure: Integer,
+                      humidity: Integer,
+                      dew_point: Float,
+                      wind_speed: Float,
+                      wind_deg: Integer,
+                      weather: Array,
+                      clouds: Integer,
+                      pop: Float,
+                      uvi: Float
+                    }
+            temp = {day: Float, min: Float, max: Float, night: Float, eve: Float, morn: Float}
+            feels_like = {day: Float, night: Float, eve: Float, morn: Float}
+            weather = {id: Integer, main: String, description: String, icon: String}
+
+            expect(forecast[:daily][0].keys).to eq(daily.keys)
+            expect(forecast[:daily][0][:temp].keys).to eq(temp.keys)
+            expect(forecast[:daily][0][:feels_like].keys).to eq(feels_like.keys)
+            expect(forecast[:daily][0][:weather][0].keys).to eq(weather.keys)
+
+            forecast[:daily].each do |day|
+              day.each do |key, value|
+                if daily[key] == Integer || daily[key] == Float
+                  expect(value.to_s.numeric?).to eq(true)
+                else
+                  expect(value.class).to eq(daily[key])
+                end
+
+                day[:temp].each do |key, value|
+                  expect(value.to_s.numeric?).to eq(true)
+                end
+
+                day[:feels_like].each do |key, value|
+                  expect(value.to_s.numeric?).to eq(true)
+                end
+
+                day[:weather][0].each do |key, value|
+                  expect(value.class).to eq(weather[key])
+                end
+              end
+            end
+          end
+
+          it 'hourly_weather' do
+            hourly = {
+                    dt: Integer,
+                    temp: Float,
+                    feels_like: Float,
+                    pressure: Integer,
+                    humidity: Integer,
+                    dew_point: Float,
+                    clouds: Integer,
+                    visibility: Integer,
+                    wind_speed: Float,
+                    wind_deg: Integer,
+                    weather: Array,
+                    pop: Integer
+                  }
+            weather = {id: Integer, main: String, description: String, icon: String}
+
+            expect(forecast[:hourly][0].keys).to eq(hourly.keys)
+            expect(forecast[:hourly][0][:weather][0].keys).to eq(weather.keys)
+
+            forecast[:hourly].each do |hour|
+              hour.each do |key, value|
+                if hourly[key] == Integer || hourly[key] == Float
+                  expect(value.to_s.numeric?).to eq(true)
+                else
+                  expect(value.class).to eq(hourly[key])
+                end
+
+                hour[:weather][0].each do |key, value|
+                  expect(value.class).to eq(weather[key])
+                end
+              end
+            end
           end
         end
       end
     end
+  end
+end
+class String
+  def numeric?
+      Float(self) != nil rescue false
   end
 end
