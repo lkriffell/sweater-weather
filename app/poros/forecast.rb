7c7142fdb8a9900df228ba7dc3ec3c831ecacfc0
Logan Riffell <lkriffell@gmail.com>
2020-11-08 11:27:58 -0700

    Refactor forecast poro and update spec

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -4,34 +4,69 @@ class Forecast
               :hourly_weather
 
   def initialize(forecast_params)
-    @current_weather = {
+    @current_weather = current(forecast_params)
+    @daily_weather = []
+    daily(forecast_params)
+    @hourly_weather = []
+    hourly(forecast_params)
+  end
+
+  def convert_date(date)
+    Time.at(date).to_date
+  end
+
+
+  def determine_wind_direction(degrees)
+    if degrees >= 330 && degrees < 30
+      "N"
+    elsif degrees >= 30 && degrees < 60
+      "NE"
+    elsif degrees >= 60 && degrees < 120
+      "E"
+    elsif degrees >= 120 && degrees < 150
+      "SE"
+    elsif degrees >= 150 && degrees < 210
+      "W"
+    elsif degrees >= 210 && degrees < 240
+      "SW"
+    elsif degrees >= 240 && degrees < 300
+      "W"
+    elsif degrees >= 300 && degrees < 330
+      "NW"
+    end
+  end
+
+  def current(forecast_params)
+    {
       dt: convert_date(forecast_params[:current][:dt]),
       sunrise: convert_date(forecast_params[:current][:sunrise]),
       sunset: convert_date(forecast_params[:current][:sunset]),
-      temp: kelvin_to_f(forecast_params[:current][:temp]),
-      feels_like: kelvin_to_f(forecast_params[:current][:feels_like]),
+      temp: forecast_params[:current][:temp],
+      feels_like: forecast_params[:current][:feels_like],
       humidity: forecast_params[:current][:humidity],
       uvi: forecast_params[:current][:uvi],
       visibility: forecast_params[:current][:visibility],
       conditions: forecast_params[:current][:weather].first[:description],
       icon: forecast_params[:current][:weather].first[:icon]
     }
+  end
 
-    @daily_weather = []
+  def daily(forecast_params)
     forecast_params[:daily].take(5).each do |forecast|
       daily_hash = {
         dt: convert_date(forecast[:dt]),
         sunrise: convert_date(forecast[:sunrise]),
         sunset: convert_date(forecast[:sunset]),
-        max_temp: kelvin_to_f(forecast[:temp][:max]),
-        min_temp: kelvin_to_f(forecast[:temp][:min]),
+        max_temp: forecast[:temp][:max],
+        min_temp: forecast[:temp][:min],
         conditions: forecast[:weather].first[:description],
         icon: forecast[:weather].first[:icon]
       }
       @daily_weather << daily_hash
     end
+  end
 
-    @hourly_weather = []
+  def hourly(forecast_params)
     forecast_params[:hourly].take(8).each do |forecast|
       hourly_hash = {
         dt: convert_date(forecast[:dt]),
@@ -43,29 +78,4 @@ class Forecast
       @hourly_weather << hourly_hash
     end
   end
-
-  def kelvin_to_f(kelvin)
-    ((kelvin - 273.15) * 9/5 + 32).round(2)
-  end
-
-  def convert_date(date)
-    Time.at(date).to_date
-  end
-
-  def determine_wind_direction(degrees)
-    if degrees >= 315 && degrees < 45
-      "North"
-    elsif degrees >= 45 && degrees < 135
-      "East"
-    elsif degrees >= 135 && degrees < 225
-      "South"
-    elsif degrees >= 225 && degrees < 315
-      "West"
-    end
-  end
 end
-# time, in a human-readable format such as “14:00:00”
-# wind_speed, string, in miles per hour
-# wind_direction, string, check wikipedia for how to convert this numeric value
-# conditions, the first ‘description’ field from the weather data as given by OpenWeather
-# icon, string, as given by OpenWeather
--- spec/poros/location_spec.rb
+++ spec/poros/location_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'location poro' do
   it 'creates a poro out of a map quest request' do
-    VCR.use_cassette 'denver_co' do
+    VCR.use_cassette 'denver_co_poro' do
       response = Faraday.get("#{ENV['MAP_URL']}/geocoding/v1/address?key=#{ENV['MAPQUEST_API_KEY']}&location=denver,co")
       location_details = JSON.parse(response.body, symbolize_names: true)
 
