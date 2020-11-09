562b0c2f405d6b80eae171b173dde9787af48d3c
Logan Riffell <lkriffell@gmail.com>
2020-11-08 20:40:50 -0700

    Update forecasts controller

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,20 +2,6 @@ class Api::V1::ForecastController < ApplicationController
   def index
     location = MapFacade.location_details(params['location'])
     forecast = ForecastFacade.forecast_details(location)
-    render json: forecast_serializer(forecast)
-  end
-
-  private
-
-  def forecast_serializer(forecast)
-    {
-      "data": {
-        "weather": {
-          "current": forecast.current_weather,
-          "daily": forecast.daily_weather,
-          "hourly": forecast.hourly_weather
-        }
-      }
-    }
+    render json: ForecastSerializer.new(forecast)
   end
 end
