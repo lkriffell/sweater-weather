46f9892410dded2263e2aedf02a9f8a3b3222d89
Logan Riffell <lkriffell@gmail.com>
2020-11-09 15:59:58 -0700

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,8 @@ ActiveRecord::Schema.define(version: 2020_11_09_210018) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
-    t.string "password_confirmation"
+    t.string "password_digest"
+    t.string "api_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
