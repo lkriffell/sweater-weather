aed60fcecc27a0841664b7e5249591740f254959
Logan Riffell <lkriffell@gmail.com>
2020-11-09 11:14:50 -0700

    Update trails_facade

--- app/facades/trails_facade.rb
+++ app/facades/trails_facade.rb
@@ -1,6 +1,12 @@
 class TrailsFacade
-  def self.trail_details(params)
-    location = MapFacade.location_details(params['location'])
-    trails = TrailsService.trail_details(location)
+  def self.trail_details(location, start_location)
+    trails = JSON.parse(TrailsService.trail_details(location).body, symbolize_names: true)
+    map_trails(trails[:trails], start_location)
+  end
+
+  def self.map_trails(trails, start_location)
+    trails.map do |trail|
+      Trail.new(trail, start_location)
+    end
   end
 end
