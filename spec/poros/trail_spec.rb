171572a1d25c4793732ed9d13e3a64742f1191b7
Logan Riffell <lkriffell@gmail.com>
2020-11-09 11:33:21 -0700

    Add trail spec

---
+++ spec/poros/trail_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'trail poro spec' do
+  it 'can create a poro out of parsed json' do
+    VCR.use_cassette 'trails.yml' do
+      location_params = { results:
+                          [  locations: [
+                            latLng: { lat: 39, lng: -105 }  ]  ]
+                        }
+      location = Location.new(location_params)
+      trails = TrailsFacade.trail_details(location, 'denver,co')
+
+      expect(trails.class).to eq(Array)
+
+      expect(trails.first.class).to eq(Trail)
+
+      trails.each do |trail|
+        expect(trail.class).to eq(Trail)
+        expect(trail.name.class).to eq(String)
+        expect(trail.summary.class).to eq(String)
+        expect(trail.difficulty.class).to eq(String)
+        expect(trail.location.class).to eq(String)
+        expect(trail.current_conditions.class).to eq(Array)
+        expect(trail.distance_to_trail.class).to eq(Route)
+      end
+    end
+  end
+end
