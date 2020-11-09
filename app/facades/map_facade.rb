45b9c4e9cfcae6be1d98d2399b7be7256d5846f2
Logan Riffell <lkriffell@gmail.com>
2020-11-09 11:14:31 -0700

    Update map facade/service to get a route

--- app/facades/map_facade.rb
+++ app/facades/map_facade.rb
@@ -4,6 +4,15 @@ class MapFacade
     create_location(JSON.parse(response.body, symbolize_names: true))
   end
 
+  def self.route_details(from, to)
+    response = MapService.route_details(from, to)
+    create_route(JSON.parse(response.body, symbolize_names: true))
+  end
+
+  def self.create_route(route)
+    Route.new(route)
+  end
+
   def self.create_location(location)
     Location.new(location)
   end
--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -1,6 +1,10 @@
 class MapService
   def self.location_details(location)
-    conn.get("/geocoding/v1/address?key=#{ENV['MAPQUEST_API_KEY']}&exclude=minutely&location=#{location}")
+    conn.get("/geocoding/v1/address?key=#{ENV['MAPQUEST_API_KEY']}&location=#{location}")
+  end
+  
+  def self.route_details(from, to)
+    conn.get("/directions/v2/route?key=#{ENV['MAPQUEST_API_KEY']}&from=#{from}&to=#{to}")
   end
 
   private
