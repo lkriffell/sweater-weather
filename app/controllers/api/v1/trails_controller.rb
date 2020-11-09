70812a7d9935ac3cba947ddf0c6010817624c3f1
Logan Riffell <lkriffell@gmail.com>
2020-11-09 11:14:56 -0700

    Update trails_controller

--- app/controllers/api/v1/trails_controller.rb
+++ app/controllers/api/v1/trails_controller.rb
@@ -1,4 +1,6 @@
-class TrailsController < ApplicationController
+class Api::V1::TrailsController < ApplicationController
   def index
+    location = MapFacade.location_details(params['location'])
+    render json: TrailSerializer.new(TrailsFacade.trail_details(location, params['location']))
   end
 end
