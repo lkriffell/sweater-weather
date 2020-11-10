4e295559e896cdb72c442268b20ee51f4e69a443
Logan Riffell <lkriffell@gmail.com>
2020-11-10 14:22:07 -0700

    Add model method spec for rounded_travel_time

--- spec/poros/road_trip_spec.rb
+++ spec/poros/road_trip_spec.rb
@@ -22,5 +22,17 @@ RSpec.describe RoadTrip do
         expect(road_trip.weather_at_eta[:conditions]).to eq("clear sky")
       end
     end
+    it 'rounded_travel_time' do
+      VCR.use_cassette 'custom_hours_forecast', :record => :none do
+        road_trip_params = {:origin=>"Denver,CO", :destination=>"Pueblo,CO", :api_key=>"jgn983hy48thw9begh98h4539h4"}
+        route = {route: {formattedTime: "01:43:57"}}
+        forecast = ForecastFacade.forecast_details_50_hours("Pueblo,CO")
+
+        road_trip = RoadTrip.new(road_trip_params, route, forecast)
+
+        expect(road_trip.travel_time).to eq("01:43:57")
+        expect(road_trip.rounded_travel_time).to eq(2)
+      end
+    end
   end
 end
