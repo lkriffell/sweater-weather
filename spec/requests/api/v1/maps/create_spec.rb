29106bf8d2c803227d6e016f83f8d2b89d6b8f46
Logan Riffell <lkriffell@gmail.com>
2020-11-10 16:46:02 -0700

    Add sad path testing for impossible routes or a bad api key

--- spec/requests/api/v1/maps/create_spec.rb
+++ spec/requests/api/v1/maps/create_spec.rb
@@ -28,5 +28,57 @@ RSpec.describe 'road_trip' do
         road_trip_structure(parsed_road_trip)
       end
     end
+    it 'cannot return a json road_trip for impossible route' do
+      VCR.use_cassette 'failed_road_trip_endpoint' do
+        User.create({
+                    email: 'email@gmail.com',
+                    password: 'password',
+                    api_key: "jgn983hy48thw9begh98h4539h4"
+                    })
+
+        params = {
+                    "origin": "Denver CO",
+                    "destination": "Paris France",
+                    "api_key": "jgn983hy48thw9begh98h4539h4"
+                  }
+
+        headers = {
+          'CONTENT_TYPE' => 'application/json',
+          'ACCEPT' => 'application/json'
+        }
+
+        post '/api/v1/road_trip', headers: headers, params: params.to_json
+
+        expect(response).to be_successful
+        parsed_error = JSON.parse(response.body, symbolize_names: true)
+        error_structure(parsed_error)
+      end
+    end
+    it 'cannot return a json road_trip with a bad api key' do
+      VCR.use_cassette 'another_failed_road_trip_endpoint' do
+        User.create({
+                    email: 'email@gmail.com',
+                    password: 'password',
+                    api_key: "jy48thw9begh98h4539h4"
+                    })
+
+        params = {
+                    "origin": "Denver CO",
+                    "destination": "Pueblo CO",
+                    "api_key": ""
+                  }
+
+        headers = {
+          'CONTENT_TYPE' => 'application/json',
+          'ACCEPT' => 'application/json'
+        }
+
+        post '/api/v1/road_trip', headers: headers, params: params.to_json
+
+        expect(response).to be_successful
+        parsed_error = JSON.parse(response.body, symbolize_names: true)
+        error_structure(parsed_error)
+      end
+    end
   end
 end
