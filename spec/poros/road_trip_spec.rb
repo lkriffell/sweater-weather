31aaaf3e3a6c4b6f1ec65ce176c6a1b0dd1ec94d
Logan Riffell <lkriffell@gmail.com>
2020-11-10 16:46:15 -0700

    Update spec

--- spec/poros/road_trip_spec.rb
+++ spec/poros/road_trip_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe RoadTrip do
     end
 
     it 'can exist' do
-      VCR.use_cassette 'custom_hours_forecast', :record => :none do
+      VCR.use_cassette 'custom_hours_forecast' do
         road_trip_params = {:origin=>"Denver,CO", :destination=>"Pueblo,CO", :api_key=>"jgn983hy48thw9begh98h4539h4"}
         route = {route: {formattedTime: "01:43:57"}}
         forecast = ForecastFacade.forecast_details_50_hours("Pueblo,CO")
@@ -18,12 +18,12 @@ RSpec.describe RoadTrip do
         expect(road_trip.end_city).to eq("Pueblo,CO")
         expect(road_trip.start_city).to eq("Denver,CO")
         expect(road_trip.travel_time).to eq("01:43:57")
-        expect(road_trip.weather_at_eta[:temperature]).to eq("50.9 F")
-        expect(road_trip.weather_at_eta[:conditions]).to eq("clear sky")
+        expect(road_trip.weather_at_eta[:temperature]).to eq("45.91 F")
+        expect(road_trip.weather_at_eta[:conditions]).to eq("few clouds")
       end
     end
     it 'rounded_travel_time' do
-      VCR.use_cassette 'custom_hours_forecast', :record => :none do
+      VCR.use_cassette 'custom_hours_forecast' do
         road_trip_params = {:origin=>"Denver,CO", :destination=>"Pueblo,CO", :api_key=>"jgn983hy48thw9begh98h4539h4"}
         route = {route: {formattedTime: "01:43:57"}}
         forecast = ForecastFacade.forecast_details_50_hours("Pueblo,CO")
