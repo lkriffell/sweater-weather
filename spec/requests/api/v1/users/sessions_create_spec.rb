1fd76417b3d8477759060ebea3698ec3968c8f2d
Logan Riffell <lkriffell@gmail.com>
2020-11-10 16:41:58 -0700

    Add sad path testing for logging in incorrectly

--- spec/requests/api/v1/users/sessions_create_spec.rb
+++ spec/requests/api/v1/users/sessions_create_spec.rb
@@ -36,5 +36,53 @@ RSpec.describe 'sessions' do
       parsed_user = JSON.parse(response.body, symbolize_names: true)
       user_structure(parsed_user)
     end
+    it 'cannot return a json user with invalid password' do
+      User.delete_all
+      User.create({
+                  email: 'email@gmail.com',
+                  password: 'password',
+                  api_key: User.generate_api_key
+                  })
+
+      params = {
+                  email: 'email@gmail.com',
+                  password: 'word'
+                }
+
+      headers = {
+        'CONTENT_TYPE' => 'application/json',
+        'ACCEPT' => 'application/json'
+      }
+
+      post '/api/v1/sessions', headers: headers, params: params.to_json
+
+      expect(response).to be_successful
+      parsed_error = JSON.parse(response.body, symbolize_names: true)
+      error_structure(parsed_error)
+    end
+    it 'cannot return a json user with invalid email' do
+      User.delete_all
+      User.create({
+                  email: 'email@gmail.com',
+                  password: 'password',
+                  api_key: User.generate_api_key
+                  })
+
+      params = {
+                  email: 'invalid@gmail.com',
+                  password: 'password'
+                }
+
+      headers = {
+        'CONTENT_TYPE' => 'application/json',
+        'ACCEPT' => 'application/json'
+      }
+
+      post '/api/v1/sessions', headers: headers, params: params.to_json
+
+      expect(response).to be_successful
+      parsed_error = JSON.parse(response.body, symbolize_names: true)
+      error_structure(parsed_error)
+    end
   end
 end
