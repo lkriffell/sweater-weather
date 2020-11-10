73ef1b05fafe3712a32eb8975d3bd75cff205c0c
Logan Riffell <lkriffell@gmail.com>
2020-11-10 16:42:12 -0700

    Add sad path testing for incorrect registration

--- spec/requests/api/v1/users/users_create_spec.rb
+++ spec/requests/api/v1/users/users_create_spec.rb
@@ -32,5 +32,50 @@ RSpec.describe 'user' do
       parsed_user = JSON.parse(response.body, symbolize_names: true)
       user_structure(parsed_user)
     end
+    it 'cannot return a json user when password dont match' do
+      User.delete_all
+
+      params = {
+                  email: 'email@gmail.com',
+                  password: 'password',
+                  password_confirmation: "word"
+                }
+
+      headers = {
+        'CONTENT_TYPE' => 'application/json',
+        'ACCEPT' => 'application/json'
+      }
+
+      post '/api/v1/users', headers: headers, params: params.to_json
+
+      expect(response).to be_successful
+      parsed_error = JSON.parse(response.body, symbolize_names: true)
+      error_structure(parsed_error)
+    end
+    it 'cannot return a json user when email already exists' do
+      User.delete_all
+      User.create({
+                  email: 'email@gmail.com',
+                  password: 'password',
+                  api_key: User.generate_api_key
+                  })
+
+      params = {
+                  email: 'email@gmail.com',
+                  password: 'password',
+                  password_confirmation: "word"
+                }
+
+      headers = {
+        'CONTENT_TYPE' => 'application/json',
+        'ACCEPT' => 'application/json'
+      }
+
+      post '/api/v1/users', headers: headers, params: params.to_json
+
+      expect(response).to be_successful
+      parsed_error = JSON.parse(response.body, symbolize_names: true)
+      error_structure(parsed_error)
+    end
   end
 end
