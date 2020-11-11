a0dc105616f0d78c0f972dc70365afe54234704c
Logan Riffell <lkriffell@gmail.com>
2020-11-10 22:18:47 -0700

    Add tests for road_trip poro

--- spec/poros/road_trip_spec.rb
+++ spec/poros/road_trip_spec.rb
@@ -18,11 +18,11 @@ RSpec.describe RoadTrip do
         expect(road_trip.end_city).to eq("Pueblo,CO")
         expect(road_trip.start_city).to eq("Denver,CO")
         expect(road_trip.travel_time).to eq("01:43:57")
-        expect(road_trip.weather_at_eta[:temperature]).to eq("45.91 F")
+        expect(road_trip.weather_at_eta[:temperature]).to eq("38.39 F")
         expect(road_trip.weather_at_eta[:conditions]).to eq("few clouds")
       end
     end
-    it 'rounded_travel_time' do
+    it 'eta' do
       VCR.use_cassette 'custom_hours_forecast' do
         road_trip_params = {:origin=>"Denver,CO", :destination=>"Pueblo,CO", :api_key=>"jgn983hy48thw9begh98h4539h4"}
         route = {route: {formattedTime: "01:43:57"}}
@@ -31,7 +31,20 @@ RSpec.describe RoadTrip do
         road_trip = RoadTrip.new(road_trip_params, route, forecast)
 
         expect(road_trip.travel_time).to eq("01:43:57")
-        expect(road_trip.rounded_travel_time).to eq(2)
+        expect(road_trip.eta).to eq(2)
+      end
+    end
+    it 'check_the_weather' do
+      VCR.use_cassette 'long_drive_forecast' do
+        road_trip_params = {:origin=>"Cozumel, Mexico", :destination=>"Toronto, Canada", :api_key=>"jgn983hy48thw9begh98h4539h4"}
+        route = {route: {formattedTime: "55:43:57"}}
+        forecast = ForecastFacade.forecast_details_50_hours("Pueblo,CO")
+
+        road_trip = RoadTrip.new(road_trip_params, route, forecast)
+
+        expect(road_trip.enough_hours?(45)).to eq(false)
+        expect(road_trip.check_the_weather(forecast)[:temperature]).to eq(road_trip.avg_temp(forecast, 2))
+        expect(road_trip.check_the_weather(forecast)[:conditions]).to eq(forecast.daily_weather[2][:conditions])
       end
     end
   end
